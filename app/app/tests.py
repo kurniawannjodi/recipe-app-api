76a920da0d11f3a445defb1a4f62d14b4a15f841
kurniawannjodi <jodikurniawan10@gmail.com>
2024-10-22 09:58:02 +0700

    Try fix

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,11 +7,12 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTest(SimpleTestCase):
+    
     """Test the calc module"""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5 ,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
